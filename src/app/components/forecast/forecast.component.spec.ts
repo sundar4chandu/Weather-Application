268728b7a251282cb2ee1d8419c672015e3c02b7
Chandra Chinta <cchinta@LT-C02S688HG8WP.local>
2019-07-15 11:21:56 -0400

    Updating unit tests.

--- src/app/components/forecast/forecast.component.spec.ts
+++ src/app/components/forecast/forecast.component.spec.ts
@@ -8,14 +8,44 @@ import { DatePipe } from '@angular/common';
 describe('ForecastComponent', () => {
   let component: ForecastComponent;
   let fixture: ComponentFixture<ForecastComponent>;
+  let weatherForecast = {
+    city: {
+      id: 1851632, name: "Shuzenji",
+      coord: { "lon": 138.933334, "lat": 34.966671 },
+      country: "JP",
+      timezone: 32400,
+      cod: "200",
+      message: 0.0045,
+      cnt: 38,
+      list: [{
+        "dt": 1406106000,
+        "main": {
+          "temp": 298.77,
+          "temp_min": 298.77,
+          "temp_max": 298.774,
+          "pressure": 1005.93,
+          "sea_level": 1018.18,
+          "grnd_level": 1005.93,
+          "humidity": 87,
+          "temp_kf": 0.26
+        },
+        "weather": [{ "id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04d" }],
+        "clouds": { "all": 88 },
+        "wind": { "speed": 5.71, "deg": 229.501 },
+        "sys": { "pod": "d" },
+        "dt_txt": "2014-07-23 09:00:00"
+      }
+      ]
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
-      declarations: [ ForecastComponent ],
-      providers: [ RestService, HttpTestingController, DatePipe  ]
+      imports: [HttpClientTestingModule],
+      declarations: [ForecastComponent],
+      providers: [RestService, HttpTestingController, DatePipe]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -28,7 +58,7 @@ describe('ForecastComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have back button', () => { 
+  it('should have back button', () => {
     const compiled = fixture.debugElement.nativeElement;
 
     expect(compiled.querySelector('button').textContent).toContain('Back');
@@ -40,5 +70,15 @@ describe('ForecastComponent', () => {
     fixture.detectChanges();
 
     expect(component.getWeatherForecast).toHaveBeenCalled();
+  });
+
+  it('should have the forecast table', () => {
+    component.weatherForecast = weatherForecast;
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('table')).toBeDefined();
   });
 });
